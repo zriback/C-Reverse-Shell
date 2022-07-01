8e582d07353a893b9e3fe8e85505a23b7fc33015
zriback <zacharythecdr@gmail.com>
2022-06-30 20:29:15 -0400

    Added interface files to handle getting info from the user

---
+++ src/main/interface.c
@@ -0,0 +1,8 @@
+#include "interface.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "shell.h"
+
+void sayHi (char * name) {
+    printf("Hello there, %s", name);
+}
---
+++ src/main/interface.h
@@ -0,0 +1,6 @@
+#ifndef INTERFACE_HEADER
+#define INTERFACE_HEADER
+
+void sayHi (char * name);
+
+#endif
