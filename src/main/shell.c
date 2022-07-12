217370779bba65d843626099d9bb4d56fb2a3c10
zriback <zacharythecdr@gmail.com>
2022-07-11 21:04:19 -0400

    Update shell.c to work with newly renamed files

--- src/main/shell.c
+++ src/main/shell.c
@@ -11,11 +11,11 @@ void createShellExecutable(struct Shell shell) {
     // use gcc to create the shell executable with the correct params
     if (shell.type == 0){
         printf("Creating bind shell executable...\n");
-        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o bshell.exe shells/bindshell.c -lws2_32", shell.port, shell.ip);
+        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o bshell.exe payloads/bindshell.c -lws2_32", shell.port, shell.ip);
     }
     else if (shell.type == 1){
         printf("Creating reverse shell executable...\n");
-        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o rshell.exe shells/reverseshell.c -lws2_32", shell.port, shell.ip);
+        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o rshell.exe payloads/reverseshell.c -lws2_32", shell.port, shell.ip);
     }
     else{
         printf("Invalid type.\n");
