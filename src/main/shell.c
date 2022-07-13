49ace01d9056b8789c823aadac72466b6d363443
zriback <zacharythecdr@gmail.com>
2022-07-13 19:16:19 -0400

    Update to also create the client exe files to pair with the shells

--- src/main/shell.c
+++ src/main/shell.c
@@ -6,22 +6,30 @@
 // Shell struct defined in .h file
 
 void createShellExecutable(struct Shell shell) {
-    char * cmd = (char*)malloc(256*sizeof(char));
+    char shellcmd[256];
+    char clientcmd[256];
+
+    system("mkdir output");
 
     // use gcc to create the shell executable with the correct params
     if (shell.type == 0){
         printf("Creating bind shell executable...\n");
-        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o bshell.exe payloads/bindshell.c -lws2_32", shell.port, shell.ip);
+        sprintf(shellcmd, "gcc -D PORT=%d -D IP=%s -o output/bshell.exe payloads/bindshell.c -lws2_32", shell.port, shell.ip);
+        printf("Creating client...\n");
+        sprintf(clientcmd, "gcc -D PORT=%d -D IP=%s -o output/bindclient.exe clients/bindshell_client.c -lws2_32", shell.port, shell.ip);
     }
     else if (shell.type == 1){
         printf("Creating reverse shell executable...\n");
-        sprintf(cmd, "gcc -D PORT=%d -D IP=%s -o rshell.exe payloads/reverseshell.c -lws2_32", shell.port, shell.ip);
+        sprintf(shellcmd, "gcc -D PORT=%d -D IP=%s -o output/rshell.exe payloads/reverseshell.c -lws2_32", shell.port, shell.ip);
+        printf("Creating client...\n");
+        sprintf(clientcmd, "gcc -D PORT=%d -D IP=%s -o output/reverseclient.exe clients/reverseshell_client.c -lws2_32", shell.port, shell.ip);
+
     }
     else{
         printf("Invalid type.\n");
     }
-    system(cmd);
-    free(cmd);
+    system(shellcmd);
+    system(clientcmd);
 
 }
 
