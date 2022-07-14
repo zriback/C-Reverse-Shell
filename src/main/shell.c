d4d489e86f25ffe7852c12e8c797a2f19b30202c
zriback <zacharythecdr@gmail.com>
2022-07-14 19:31:21 -0400

    Small formatting change

--- src/main/shell.c
+++ src/main/shell.c
@@ -16,13 +16,13 @@ void createShellExecutable(struct Shell shell) {
         printf("Creating bind shell executable...\n");
         sprintf(shellcmd, "gcc -D PORT=%d -D IP=%s -o output/bshell.exe payloads/bindshell.c -lws2_32", shell.port, shell.ip);
         printf("Creating client...\n");
-        sprintf(clientcmd, "gcc -D PORT=%d -D IP=%s -o output/bindclient.exe clients/bindshell_client.c -lws2_32", shell.port, shell.ip);
+        sprintf(clientcmd, "gcc -D PORT=%d -D IP=%s -o output/bclient.exe clients/bindshell_client.c -lws2_32", shell.port, shell.ip);
     }
     else if (shell.type == 1){
         printf("Creating reverse shell executable...\n");
         sprintf(shellcmd, "gcc -D PORT=%d -D IP=%s -o output/rshell.exe payloads/reverseshell.c -lws2_32", shell.port, shell.ip);
         printf("Creating client...\n");
-        sprintf(clientcmd, "gcc -D PORT=%d -D IP=%s -o output/reverseclient.exe clients/reverseshell_client.c -lws2_32", shell.port, shell.ip);
+        sprintf(clientcmd, "gcc -D PORT=%d -D IP=%s -o output/rclient.exe clients/reverseshell_client.c -lws2_32", shell.port, shell.ip);
 
     }
     else{
